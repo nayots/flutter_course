6a84d65a08aecf418451f64fb400bd3c7897f066
Stoyan Grigorov <grigorov.stoyan@gmail.com>
2019-11-21 00:09:56 +0200

    📦 NEW: adds final changes to dice app 🎲

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,28 +17,36 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void onDicePressed() {
+    setState(() {
+      leftDiceNumber = Random.secure().nextInt(6) + 1;
+      rightDiceNumber = Random.secure().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    var leftDiceNumber = 5;
-    var rightDiceNumber = 1;
-
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
             child: FlatButton(
-              onPressed: () {
-                print('Left');
-              },
+              onPressed: onDicePressed,
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
             child: FlatButton(
-              onPressed: () {
-                print('Right');
-              },
+              onPressed: onDicePressed,
               child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
