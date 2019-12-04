11680d11472b1562981378109f086ea28c77d342
Stoyan Grigorov <grigorov.stoyan@gmail.com>
2019-12-04 22:55:01 +0200

    📦 NEW: adds xylophone additional features

--- xylophone-flutter/lib/main.dart
+++ xylophone-flutter/lib/main.dart
@@ -4,19 +4,42 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  List colors = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.teal,
+    Colors.blue,
+    Colors.purple
+  ];
+
+  void playSound(int noteNumber) {
+    final player = AudioCache();
+    player.play('note$noteNumber.wav');
+  }
+
+  Widget buildKey(int index) {
+    final button = FlatButton(
+      child: null,
+      color: colors[index],
+      onPressed: () {
+        playSound(index + 1);
+      },
+    );
+    return Expanded(child: button);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-              child: FlatButton(
-            onPressed: () {
-              final player = AudioCache();
-              player.play('note1.wav');
-            },
-            child: Text('Click me'),
-          )),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: List.generate(7, buildKey),
+          ),
         ),
       ),
     );
